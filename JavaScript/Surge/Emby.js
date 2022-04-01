74783804972446f5e77e4e089faabb46ceafd4e1
Godrock <paulstoker@163.com>
2022-04-02 00:10:12 +0800

    Update Emby.js

--- JavaScript/Surge/Emby.js
+++ JavaScript/Surge/Emby.js
@@ -1,66 +1,3 @@
-# > 流媒体emby-nexitally服务器
-DOMAIN,emby.nexitally.com
-
-# > 流媒体emby-paoche服务器
-DOMAIN,emby.cjpc.cc
-
-# > 流媒体emby-flowercloud服务器
-DOMAIN,emby.flowercloud.yt
-
-# > 流媒体emby-maying服务器
-DOMAIN,emby.maying.club
-
-# > 流媒体emby-mdss服务器
-DOMAIN,emby.mdss.cloud
-
-# > 流媒体emby-ytoo服务器
-DOMAIN,emby.ytoo.li
-
-# > 流媒体emby-w8ves服务器
-DOMAIN,embyserver.ga
-
-# > 流媒体emby-prpr服务器
-DOMAIN,emby.prprcloud.com
-
-# > 流媒体emby-nyancat服务器
-DOMAIN,emby.nyancat.net
-
-# > 流媒体emby-Plus体验服
-DOMAIN,emby.plusmedia.site
-DOMAIN,emby-cf.plusmedia.site 
-DOMAIN,emby.xeton.dev
-DOMAIN,jellyfin.xeton.dev
-DOMAIN,movie.xeton.dev
-DOMAIN,servers.xeton.dev
-
-# > 流媒体emby-Otaku体验服
-DOMAIN,emby.otakudrive.life
-DOMAIN,emby2.otakudrive.life 
-
-# > 流媒体pornemby服务器
-DOMAIN,pornemby.club
-
-# > 流媒体IMM服务器
-DOMAIN,emby.immtel.com
-
-# > 流媒体Exflux服务器
-DOMAIN,line.emby.men
-
-# > 流媒体SoCloud服务器
-DOMAIN,emby.run
-
-# > 流媒体普拉斯影业服务器
-DOMAIN,emby.plusmedia.site
-DOMAIN,emby-sg.plusmedia.site
-DOMAIN,emby-cf.plusmedia.site
-DOMAIN,emby.xeton.dev
-DOMAIN,jellyfin.xeton.dev
-DOMAIN,movie.xeton.dev
-DOMAIN,servers.xeton.dev
-
-# > 流媒体WaveCloud服务器
-DOMAIN,embyserver.ga
-
 # > 流媒体卷毛鼠服务器
 DOMAIN,cf.jmsooo.com
 DOMAIN,sg1.jmsooo.com
@@ -68,20 +5,16 @@ DOMAIN,sg2.jmsooo.com
 DOMAIN,usa1.jmsooo.com
 DOMAIN,usa2.jmsooo.com
 
-# > 流媒体Odyssey服务器
+# > 流媒体Odyssey+服务器
 DOMAIN,cf.odysseyplus.site
 DOMAIN,sg.odysseyplus.site
 DOMAIN,jp.odysseyplus.site
 DOMAIN,ru.odysseyplus.site
-DOMAIN,sp.odysseyplus.site
 DOMAIN,us.odysseyplus.site
 DOMAIN,kr.odysseyplus.site
 
-# > 流媒体Charon  TV +服务器
+# > 流媒体CharonTV服务器
 DOMAIN,play.charontv.com
 
-# > 流媒体熊猫影业服务器
-DOMAIN,emby.kim
-
-# > 流媒体MisakaF_Emby服务器
+# > 流媒体MisakaF服务器
 DOMAIN,emby.misakaf.xyz
